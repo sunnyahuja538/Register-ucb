84929baa1be4ef96e5557977b452837317151765
sunnyahuja538 <sunnyahuja538@gmail.com>
2025-05-18 22:57:12 +0530

    fix loading page

--- Frontend/src/pages/Form.jsx
+++ Frontend/src/pages/Form.jsx
@@ -143,7 +143,7 @@ useEffect(()=>{
     }
     })()
     },[employeeFirstName]);
-    return (<>{(processing)?<img className='ml-[45%] mt-[18%] h-[10%] w-[10%]' src="https://assets-v2.lottiefiles.com/a/91cc0ece-1150-11ee-b7cb-d3afb5c0c001/QNF78Uk4YE.gif"/>:
+    return (<>{(processing)?<div className='h-full w-full flex justify-center'><img className='h-[10%] w-[10%] mt-[18%]' src="https://assets-v2.lottiefiles.com/a/91cc0ece-1150-11ee-b7cb-d3afb5c0c001/QNF78Uk4YE.gif"/></div>:
         <div className="min-h-screen flex items-center justify-center bg-gray-300">
             <form onSubmit={submit} className="bg-gray-100 p-8 rounded-2xl shadow-2xl w-full max-w-lg">
                 <h1 className="text-2xl font-semibold text-center mb-8 text-gray-800 underline decoration-gray-500">Enter Your Details</h1>
